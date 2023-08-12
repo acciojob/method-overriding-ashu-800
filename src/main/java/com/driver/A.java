bc89844086b9e037c4c1ab9ceccf629671115775
Ashutosh <ashushukla800@gmail.com>
2023-08-12 13:12:23 +0530

    Attempt2

--- src/main/java/com/driver/A.java
+++ src/main/java/com/driver/A.java
@@ -5,3 +5,9 @@ public class A {
        return "Invoking method from class A";
    }
 }
+
+class B extends A{
+    public String meth(){
+        return "Method is overridden in Extendend class B";
+    }
+}
